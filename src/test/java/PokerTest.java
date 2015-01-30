c1c5f40d339932b338971bbfc48a99b3143adb69
vlad <vlad@v-lad.org>
2015-01-29 19:34:41 -0500

    Started

---
+++ src/test/java/PokerTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.List;
+
+
+public class PokerTest {
+
+
+    @Before
+    public void setUp()
+    {
+
+    }
+
+    @Test
+    public void testThatWeHaveSixItemsInTheInventory()
+    {
+        assertEquals(6, 6);
+    }
+
+
+
+}
